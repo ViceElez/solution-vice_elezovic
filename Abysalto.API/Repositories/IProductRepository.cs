f30b51cb09d8c4d7c488b397b82cf08ede2c3b40
ViceElez <vice.elezovic12@gmail.com>
2026-07-02 00:59:24 +0200

    create interface and repository

--- Abysalto.API/Repositories/IProductRepository.cs
+++ Abysalto.API/Repositories/IProductRepository.cs
@@ -4,5 +4,9 @@ namespace Abysalto.API.Repositories
 {
     public interface IProductRepository
     {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int productId);
+        IEnumerable<Product> GetProductsByCategoryAndPrice(string category, float price);
+        Product GetProductByName(string name);
     }
 }
